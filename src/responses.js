d4d54f49c2125e77d8149ff18f69f91dfbac3018
Jake Recoon <jrecoon@gmail.com>
2017-09-26 19:02:48 -0400

    added files working except for HEAD

---
+++ src/responses.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const css = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+const users = {};
+
+const respond = (request, response, content, type) => {
+  response.writeHead(200, { 'Content-Type': type });
+  response.write(content);
+  response.end();
+};
+
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
+
+  respondJSON(request, response, 200, responseJSON);
+};
+
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required',
+  };
+
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201;
+
+  if (users[body.name]) {
+    responseCode = 204;
+  } else {
+    users[body.name] = {};
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode);
+};
+
+const getIndex = (request, response) => {
+  respond(request, response, index, 'text/html');
+};
+
+const getCSS = (request, response) => {
+  respond(request, response, css, 'text/css');
+};
+
+const notFound = (request, response) => {
+  respondJSONMeta(request, response, 404);  
+};
+
+module.exports = {
+  getIndex,
+  getCSS,
+  getUsers,
+  addUser,
+  notFound,
+};
